d67a0537acd7d0d9e1ed64f87fbe2867c65d2a74
U-francine\marc <marc@fogtest.com>
2014-10-05 18:17:23 -0700

    added starter cherrypy website

--- fabfile.py
+++ fabfile.py
@@ -18,7 +18,7 @@ from local_settings import AWS_SECRET_ACCESS_KEY
 
 
 now = datetime.now()
-
+INSTANCE_ID='i-d37496d9'
 
 RSYNC_ARGS = '-vrz  --delete --exclude=*.pyc'
 
@@ -38,6 +38,36 @@ def sync():
     copy local changes in ~/personal/chef to sfgeek.net:rails_apps/rrg_chef using rsync
     :return:
     """
-    cmd =  'rsync -ah --delete  --exclude="*/tmp/*"   --exclude=".git/*"  %s marc@sfgeek.net:python_apps/coconuts'%(SRC)
+    cmd =  'rsync -i ~/aws-centos-db-2.pem -ah --delete  --exclude="*/tmp/*"   --exclude=".git/*"  %s marc@coconuts.sfblur.com:python_apps/coconuts'%(SRC)
     local(cmd)
 
+
+def start():
+    """
+    start - start the AWS Packaging Server - run local
+    :return:
+    """
+    local('aws ec2 start-instances --instance-ids %s'%(AWS_INSTANCE_ID))
+
+def stop():
+    """
+    stop - stop the AWS Packaging Server - run local
+    :return:
+    """
+    local('aws ec2 stop-instances --instance-ids %s'%(AWS_INSTANCE_ID))
+
+def echo_ip():
+    """
+    echo_ip - report Public IP of AWS Packaging Server - run local
+    :return:
+    """
+    ec2conn = connect_to_region('us-west-2',
+                  aws_access_key_id=AWS_ACCESS_KEY_ID,
+                  aws_secret_access_key=AWS_SECRET_ACCESS_KEY)
+    reservations = ec2conn.get_all_instances()
+    #print reservations.AWS_INSTANCE_ID
+
+    instances = [i for r in reservations for i in r.instances]
+    for i in instances:
+        if i.id == AWS_INSTANCE_ID:
+            pprint(i.ip_address)
